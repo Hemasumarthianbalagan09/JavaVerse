f59df355ce064463f721655ab7f70f45055c5c8f
Hemasumarthianbalagan09 <hemasumarthi2006@gmail.com>
2026-09-22 15:29:42 +0530

    Added palindrome program

---
+++ hashmap.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+public class hashmap {
+    public static void main(String[] args) {
+        HashMap<Integer,String> students = new HashMap<>();
+        students.put(101,"Selva");
+        students.put(102,"Arun");
+        students.put(103,"hemso");
+        System.out.println(students);
+    }
+}
+    
+
